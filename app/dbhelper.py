23c59ded90c5490c563da815c6efd1e2fd5715de
Haroun <harounchahed1996@gmail.com>
2018-06-24 01:51:30 +0800

    added database integration

---
+++ app/dbhelper.py
@@ -0,0 +1,43 @@
+import sqlite3
+
+class DBHelper:
+    def __init__(self, dbname="calendar.db"):
+        self.dbname = dbname
+        self.conn = sqlite3.connect(dbname) 
+    '''
+    def setup(self):
+        #tblstmt = "CREATE TABLE IF NOT EXISTS items (description text, owner text)"db.conn.execute("create table if not exists items (description text, owner text")
+        self.conn.execute("create table if not exists items (description text, owner text)")
+        itemidx = "CREATE INDEX IF NOT EXISTS itemIndex ON items (description ASC)" 
+        ownidx = "CREATE INDEX IF NOT EXISTS ownIndex ON items (owner ASC)"
+        #self.conn.execute(tblstmt)
+        self.conn.execute(itemidx)
+        self.conn.execute(ownidx)
+        self.conn.commit()
+    '''
+    def add_item(self, user_id, event_id, start_date, end_date):
+        stmt = "INSERT INTO cal (user_id, event_id, start_date, end_date) VALUES (?, ?, ?, ?)"
+        args = (user_id, event_id, start_date, end_date)
+        self.conn.execute(stmt, args)
+        self.conn.commit()
+    def delete_item(self, user_id, event_id, start_date, end_date):
+        stmt = "DELETE FROM cal WHERE user_id = (?), event_id = (?), start_date = (?), end_date = (?)" 
+        args = (user_id, event_id, start_date, end_date)
+        self.conn.execute(stmt, args)
+        self.conn.commit()
+    def get_schedule(self, user_id):
+        stmt = "SELECT start_date, end_date FROM cal WHERE user_id = (?)"
+        args = (user_id)
+        return [[x[0],x[1]] for x in self.conn.execute(stmt, args)]
+    def get_free_time(self, user_id, small_limit = None , big_limit = None):
+        if small_limit = None and big_limit = None:
+            busy_time = get_schedule(user_id)
+            busy_time = busy_time.sort(key=lambda x: x[0])
+            free_time = [] 
+            for i in range(len(busy_time)-1):
+                free_time.append([busy_time[i][1], busy_time[i+1][0])
+            free_time.appen([busy_time[-1][1],busy_time[0][0]])
+
+        
+
+db = DBHelper()
--- app/noble_Sheet_Bot.py
+++ app/noble_Sheet_Bot.py
@@ -1,120 +1,35 @@
-# NOBLESHEET BOT
-# coding: utf-8
-
-# In[4]:
-
-
-# https://khashtamov.com/en/how-to-deploy-telegram-bot-django/
-# Communicate with Telegram service periodically - provide special URL to your bot when an event occurs
-    # using API call getUpdates
-    # setting up a Webhook 
-        # option 1: obtain a valid SSL certificate 
-        # option 2: generate self-signed certificate using tools like OpenSSL
-        
-
-import telepot
-token = '491905862:AAFLeZ9-H56wumXpGN0LoyUN_1Dmm751nH8'
-TelegramBot = telepot.Bot(token)
-print (TelegramBot.getMe())
-
-# alternatively
-# https://api.telegram.org/bot<your-bot-token>/getme
-
-
-# In[5]:
-
-
-# Getting text details
-    # update_id is tied to the message id (increases in chronological order)
-
-TelegramBot.getUpdates()
-
-
-# In[69]:
-
-
-# Helper code
-import sqlite3
-
-# To-do list code
-class DBHelper:
-    def __init__(self, dbname="todo.sqlite"): #takes a database name and creates a database connection
-        self.dbname = dbname
-        self.conn = sqlite3.connect(dbname)
-
-    def setup(self): #creates a new table
-        stmt = "CREATE TABLE IF NOT EXISTS items (description text)"
-        self.conn.execute(stmt)
-        self.conn.commit()
-
-    def add_item(self, item_text): #takes the text for the item and inserts it into our database 
-        stmt = "INSERT INTO items (description) VALUES (?)"
-        args = (item_text, )
-        self.conn.execute(stmt, args)
-        self.conn.commit()
-
-    def delete_item(self, item_text): #takes the text for an item and removes it
-        stmt = "DELETE FROM items WHERE description = (?)"
-        args = (item_text, )
-        self.conn.execute(stmt, args)
-        self.conn.commit()
-
-    def get_items(self): #returns a list of all the items in our database
-        stmt = "SELECT description FROM items"
-        return [x[0] for x in self.conn.execute(stmt)]
-    
-
-
-# In[74]:
-
-
-# 1. Initialize
-
-import json 
+import json
 import requests
 import time
 import urllib
-import configparserimport io, os, sys, types
-from IPython import get_ipython
-from nbformat import read
-from IPython.core.interactiveshell import InteractiveShell
-
-
-# In[66]:
-
+from dbhelper import DBHelper
 
 db = DBHelper()
+
 TOKEN = '491905862:AAFLeZ9-H56wumXpGN0LoyUN_1Dmm751nH8'
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 
 
-# downloads from url and returns a string
-# no exceptions used for simplicity
 def get_url(url):
     response = requests.get(url)
     content = response.content.decode("utf8")
     return content
 
 
-# parses string response into a python dictionary
 def get_json_from_url(url):
     content = get_url(url)
     js = json.loads(content)
     return js
 
 
-# calls api command to retrieve list of updates
-    # offset parameter to indicate the bot will reject messages with lower ids
-    # timeout argument added
 def get_updates(offset=None):
-    url = URL + "getUpdates?timeout=100"
+    url = URL + "getUpdates"
     if offset:
-        url += "&offset={}".format(offset)
+        url += "?offset={}".format(offset)
     js = get_json_from_url(url)
     return js
 
 
-# loops through each update to return the largest id
 def get_last_update_id(updates):
     update_ids = []
     for update in updates["result"]:
@@ -122,7 +37,30 @@ def get_last_update_id(updates):
     return max(update_ids)
 
 
-# get chat id and message text
+def handle_updates(updates):
+    for update in updates["result"]:
+        text = update["message"]["text"]
+        chat = update["message"]["chat"]["id"]
+        items = db.get_items(chat)
+        if text == "/done":
+            keyboard = build_keyboard(items)
+            send_message("Select an item to delete", chat, keyboard)
+        elif text == "/start":
+            send_message("Welcome to your personal To Do list. Send any text to me and I'll store it as an item. Send /done to remove items", chat)
+        elif text.startswith("/"):
+            continue
+        elif text in items:
+            db.delete_item(text, chat)
+            items = db.get_items(chat)
+            keyboard = build_keyboard(items)
+            send_message("Select an item to delete", chat, keyboard)
+        else:
+            db.add_item(text, chat)
+            items = db.get_items(chat)
+            message = "\n".join(items)
+            send_message(message, chat)
+
+
 def get_last_chat_id_and_text(updates):
     num_updates = len(updates["result"])
     last_update = num_updates - 1
@@ -131,87 +69,30 @@ def get_last_chat_id_and_text(updates):
     return (text, chat_id)
 
 
-# Function 1: Echo reply for each message received
-# use another notebook for this
-# def echo_all(updates):
-#     for update in updates["result"]:
-#         try:
-#             text = update["message"]["text"]
-#             chat = update["message"]["chat"]["id"]
-#             send_message(text, chat)
-#         except Exception as e:
-#             print(e)
+def build_keyboard(items):
+    keyboard = [[item] for item in items]
+    reply_markup = {"keyboard":keyboard, "one_time_keyboard": True}
+    return json.dumps(reply_markup)
 
 
-# In[67]:
-
-
-# Function 2: To-Do List
-def handle_updates(updates):
-    for update in updates["result"]:
-        try:
-            text = update["message"]["text"]
-            chat = update["message"]["chat"]["id"]
-            items = DBHelper().get_items()
-            if text in items:
-                DBHelper().delete_item(text)
-                items = DBHelper().get_items()
-            else:
-                DBHelper().add_item(text)
-                items = DBHelper().get_items()
-            message = "\n".join(items)
-            send_message(message, chat)
-        except KeyError:
-            pass
-
-# modified to read intervals
-def send_message(text, chat_id):
+def send_message(text, chat_id, reply_markup=None):
     text = urllib.parse.quote_plus(text)
-    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+    url = URL + "sendMessage?text={}&chat_id={}&parse_mode=Markdown".format(text, chat_id)
+    if reply_markup:
+        url += "&reply_markup={}".format(reply_markup)
     get_url(url)
-    
 
-# manual reply
-# text, chat = get_last_chat_id_and_text(get_updates())
-# send_message(text, chat)
-
-
-# In[68]:
-
-
-# Update with instant response
- 
-db.setup()
 
 def main():
+    db.setup()
     last_update_id = None
     while True:
         updates = get_updates(last_update_id)
         if len(updates["result"]) > 0:
             last_update_id = get_last_update_id(updates) + 1
-            echo_all(updates)
+            handle_updates(updates)
         time.sleep(0.5)
 
 
 if __name__ == '__main__':
     main()
-
-
-# In[32]:
-
-
-rude = ["Oi", "hey bitch", "Fuck you"]
-nice = ["hello", "hi", "hey beautiful"]
-
-
-# In[35]:
-
-
-text, chat = get_last_chat_id_and_text(get_updates())
-if text in rude:
-    send_message("Eh fuck u", chat)
-elif text in nice:
-    send_message("hey sweetheart", chat)
-elif text not in rude or nice:
-    send_message("sorry repeat", chat)
-
